06313729eaaadd97954227f3437e5e921e5eb212
azamatif <azamatif@edu.code.az>
2025-04-24 01:42:02 +0200

    Home task has e

---
+++ Variables-DataTypes-Algoritm/Variables-DataTypes-Algoritm/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region
+int num = 5;
+if (num % 3 == 0)
+{
+    Console.WriteLine("eded 3 e bolunur");
+}
+else
+{
+    Console.WriteLine("eded 3 e bolunmur");
+}
+#endregion
+#region
+int n = 30;
+if (n % 3 == 0 && n % 7 == 0)
+{
+    Console.WriteLine("eded 3 ve 7 ye bolunur");
+}
+else
+{
+    Console.WriteLine("eded 3 ve 7 ye bolunmur");
+}
+
+int no = 12;
+if (no > 10 && no < 15)
+{
+    Console.WriteLine("eded 10 dan boyuk, 15 den kicikdir");
+}
+else
+{
+    Console.WriteLine("Eded bu araligda yoxdur");
+}
+#endregion
+#region
+int num = 5;
+if (num % 3 == 0)
+{
+    Console.WriteLine("eded 3 e bolunur");
+}
+else
+{
+    Console.WriteLine("eded 3 e bolunmur");
+}
+#endregion
